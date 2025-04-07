39ff19b770410bacf5f5aebce6e27b959b8d9f9d
heobae <heowling94@gmail.com>
2025-04-07 17:55:33 +0900

    0407 학원

--- 사용자계정script.sql
+++ 사용자계정script.sql
@@ -10,13 +10,13 @@
 ALTER SESSION SET "_ORACLE_SCRIPT" = TRUE;
 
 -- 계정 생성 구문 (username : kh / password : kh1234)
-CREATE USER todo IDENTIFIED BY 1234;
+CREATE USER todo_jdbc IDENTIFIED BY todoList1234;
 
-GRANT RESOURCE, CONNECT TO todo;
+GRANT RESOURCE, CONNECT TO todo_jdbc;
 -- 사용자 계정 권한 부여 설정
 -- RESOURCE : 테이블이나 인덱스같은 DB 객체를 생성할 권한
 -- CONNECT : DB에 연결하고 로그인 할 수 있는 권한
 
-ALTER USER todo DEFAULT TABLESPACE SYSTEM QUOTA UNLIMITED ON SYSTEM ;
+ALTER USER todo_jdbc DEFAULT TABLESPACE SYSTEM QUOTA UNLIMITED ON SYSTEM ;
 -- 객체가 생성될 수 있는 공간 할당량 무제한 지정
 
